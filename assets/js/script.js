26fccec89a2d8ddb375f17dfd4f38969db39bedb
Jonatas Leon <jonatas.leon@gmail.com>
2016-01-23 17:12:57 -0200

    background image position fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
     $("#next-page").click(function() {
         $("html, body").animate({
             scrollTop: $("article#main").offset().top
-        }, 2000);
+        }, 1000);
     });
 });
