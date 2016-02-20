5d4b672c9ecd5952e466c2fb072c3fb893b32a40
Jonatas Leon <jonatas.leon@gmail.com>
2016-02-20 12:56:07 -0200

    Homepage removed

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ angular.module("myApp", ["firebase", "ngRoute", "ngAnimate"]);
 
 angular.module("myApp").config(function ($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl: "./view/homepage.html",
-        })
         .when("/about", {
             templateUrl: "./view/about.html",
             controller: "AboutCtrl",
