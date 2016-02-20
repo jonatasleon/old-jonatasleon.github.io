531d732e2019d5f6863a75f45d4bfdf89b5a183a
Jonatas Leon <jonatas.leon@gmail.com>
2016-02-20 13:38:43 -0200

    Refactory firebaseRefService.js

--- js/services/firebaseRefService.js
+++ js/services/firebaseRefService.js
@@ -1,21 +1,27 @@
 angular.module("myApp").factory("firebaseRef", ["config", "$firebaseObject", "$firebaseArray",
     function (config, $firebaseObject, $firebaseArray) {
-        var _ref = new Firebase(config.baseURL),
-            _resolveRef = function (parentRef, childName, returnType) {
-                return returnType(parentRef.child(childName));
-            },
-            _resolveRefObject = function (parentRef, childName) {
-                return _resolveRef(parentRef, childName, $firebaseObject);
-            },
-            _resolveRefArray = function (parentRef, childName) {
-                return _resolveRef(parentRef, childName, $firebaseArray);
-            },
-            _about = function () {
-                return _resolveRefObject(_ref, config.aboutChild);
-            },
-            _messages = function () {
-                return _resolveRefArray(_ref, config.messagesChild);
-            };
+        var _ref = new Firebase(config.baseURL);
+
+        function _about() {
+            return _resolveRefObject(_ref, config.aboutChild);
+        }
+
+        function _messages() {
+            return _resolveRefArray(_ref, config.messagesChild);
+        }
+
+        function _resolveRefObject(parentRef, childName) {
+            return _resolveRef(parentRef, childName, $firebaseObject);
+        }
+
+        function _resolveRefArray(parentRef, childName) {
+            return _resolveRef(parentRef, childName, $firebaseArray);
+        }
+
+        function _resolveRef(parentRef, childName, callbackType) {
+            return callbackType(parentRef.child(childName));
+        }
+
         return {
             getAbout: _about,
             getMessages: _messages
