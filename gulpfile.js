aa3fd638d5b788d5de793752f94116490f3da1cf
Jonatas Leon <jonatas.leon@gmail.com>
2016-02-23 23:00:08 -0300

    Html resolve improved

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     concatCss = require('gulp-concat-css'),
-    htmlReplace = require('gulp-html-replace'),
+    gulpif = require('gulp-if'),
+    useref = require('gulp-useref'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     runSequence = require('run-sequence');
@@ -11,7 +12,7 @@ var gulp = require('gulp'),
 gulp.task('default', ['make-build']);
 
 gulp.task('make-build', function() {
-    runSequence('img-min', 'build-js', 'build-css', 'html-replace', 'move-files');
+    runSequence('img-min', 'resolve-assets', 'move-files');
 });
 
 gulp.task('img-min', function() {
@@ -26,41 +27,11 @@ gulp.task('img-min', function() {
         .pipe(gulp.dest('./img/dest/'));
 });
 
-gulp.task('build-js', function() {
-    var filesToConcat = [
-        './lib/firebase/firebase.js',
-        './lib/angular/angular.js',
-        './lib/angularfire/dist/angularfire.js',
-        './lib/angular-route/angular-route.js',
-        './lib/angular-animate/angular-animate.js',
-        './js/app.js',
-        './js/controllers/*.js',
-        './js/services/*.js',
-        './js/value/*.js'
-    ];
-    return gulp.src(filesToConcat)
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('build/js'));
-});
-
-gulp.task('build-css', function() {
-    var filesToConcat = [
-        './css/reset.css',
-        './lib/flexboxgrid/dist/flexboxgrid.css',
-        './css/style.css',
-        './css/ngstyle.css'
-    ];
-    return gulp.src(filesToConcat)
-        .pipe(concatCss('style.css'))
-        .pipe(gulp.dest('build/css'));
-});
-
-gulp.task('html-replace', function() {
+gulp.task('resolve-assets', function() {
     return gulp.src('./index.html')
-        .pipe(htmlReplace({
-            'js': 'js/app.js',
-            'css': 'css/style.css'
-        }))
+        .pipe(useref())
+        .pipe(gulpif('*.css', concatCss('css/myappstyle.css')))
+        .pipe(gulpif('*.js', concat('js/myappscript.js')))
         .pipe(gulp.dest('build'));
 });
 
