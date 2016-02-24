163347dde37eac55cf429da6d5d594b4445ba873
Jonatas Leon <jonatas.leon@gmail.com>
2016-02-23 23:19:15 -0300

    Code cleaned

--- js/controllers/contactCtrl.js
+++ js/controllers/contactCtrl.js
@@ -1,10 +1,16 @@
 angular.module("myApp").controller("ContactCtrl", ["$scope", "addMessage",
     function ($scope, addMessage) {
-        addMessage({author: "Jonatas", content: "test", dateTime: "2016-02-02T10:12:28-02:00", email: "", subject: ""}).then(function (ref) {
+        var fakeMessage = {author: "Jonatas", content: "test", dateTime: "2016-02-02T10:12:28-02:00", email: "", subject: ""};
+
+        function onSuccess(ref) {
             var id = ref.key();
             console.log(id);
-        }, function () {
+        }
+
+        function onError() {
             console.log("LASCOU-SE");
-        });
+        }
+
+        addMessage(fakeMessage).then(onSuccess, onError);
     }
 ]);
