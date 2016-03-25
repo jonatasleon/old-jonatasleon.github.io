d0c4d662bc8ecb4e3d95097fc8d71f861d7f5573
Jonatas Leon <jonatas.leon@gmail.com>
2016-03-25 15:52:48 -0300

    Restructured

--- gulpfile.js
+++ gulpfile.js
@@ -2,53 +2,27 @@
 
 var gulp = require('gulp'),
     runSequence = require('run-sequence'),
-    del = require('del'),
     eslint = require('gulp-eslint'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     useref = require('gulp-useref'),
     gulpif = require('gulp-if'),
     concat = require('gulp-concat'),
-    concatCss = require('gulp-concat-css');
+    concatCss = require('gulp-concat-css'),
+    cssmin = require('gulp-csso'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', ['make-build']);
 
 gulp.task('make-build', function() {
-    runSequence('del-build', 'eslint', 'image-min', 'concat-files', 'copy-files');
-});
-
-gulp.task('image-min', function() {
-    return gulp.src('./img/src/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./img/dest/'));
-});
-
-gulp.task('concat-files', function() {
-    return gulp.src('./index.html')
-        .pipe(useref())
-        .pipe(gulpif('*.css', concatCss('css/styles.css')))
-        .pipe(gulpif('*.js', concat('js/scripts.js')))
-        .pipe(gulp.dest('build'));
-});
-
-gulp.task('copy-files', function() {
-    var filesToMove = [
-        '.travis.yml',
-        './CNAME',
-        './README.md',
-        './view/**/*.html',
-        './img/dest/**/*.{png,jpg}'
-    ];
-    return gulp.src(filesToMove, {
-            base: './'
-        })
-        .pipe(gulp.dest('build'));
+    runSequence(
+        'eslint',
+        'image-min',
+        'concat-files',
+        'css-min',
+        'html-min',
+        'copy-files'
+    );
 });
 
 gulp.task('eslint', function() {
@@ -72,6 +46,50 @@ gulp.task('eslint', function() {
         .pipe(eslint.failOnError());
 });
 
-gulp.task('del-build', function() {
-    return del(['./build/']);
+gulp.task('image-min', function() {
+    return gulp.src('./img/src/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./img/dest/'));
+});
+
+gulp.task('concat-files', function() {
+    return gulp.src('./index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.css', concatCss('css/styles.css')))
+        .pipe(gulpif('*.js', concat('js/scripts.js')))
+        .pipe(gulp.dest('build/assets/'));
+});
+
+gulp.task('css-min', function() {
+    return gulp.src('./build/css/styles.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('html-min', function() {
+    return gulp.src('./view/**/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('./build/view/'));
+});
+
+gulp.task('copy-files', function() {
+    var filesToMove = [
+        '.travis.yml',
+        './CNAME',
+        './README.md',
+        './img/dest/**/*.{png,jpg}'
+    ];
+    return gulp.src(filesToMove, {
+            base: './'
+        })
+        .pipe(gulp.dest('build'));
 });
